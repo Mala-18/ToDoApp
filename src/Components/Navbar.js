85ce6d2acd2e94a45d2ac54682845fe4576266e1
Mala H R <malahr@Malas-MacBook-Pro.local>
2022-12-07 17:56:27 +0530

    [Mala] Add Task functionality is added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,22 @@
 import "./Navbar.css"
 import ItemsList from "./ItemsList";
-// import 'reactjs-popup/dist/index.css';
 import {useState} from 'react'
 import itemsList from "../TaskList"
-import Popup from "reactjs-popup";
 
 function Navbar(){
     const [title,setTitle] = useState("");
     const [status,setStatus] = useState("");
+    const [popup,setPopup] = useState(false);
 
+    const handleButtonClick = () => {
+        setPopup(!popup);
+    }
+
+    const closePopup = () => {
+        setPopup(false);
+    }
     const handleSubmit = event =>{
+        setPopup(false)
         const date= new Date();
 
         const currentDate= date.getDay()+"/"+date.getMonth()+"/"+date.getFullYear();
@@ -22,37 +29,13 @@ function Navbar(){
 
         itemsList.push({text : title, dateAndTime : currentTime+" "+currentDate})
 
-        console.log(itemsList);
+        
     }
 
     return(
         <div className="navbar">
-
-            {/* <Popup className="navbar-popup-box" trigger={<button className="navbar-btn" closeOnEscape> Add Task </button>}>
-
-                <div className="navbar-popup">
-                    <h2>Add Task</h2>
-                    <label className="navbar-btn-title"> Title
-                        <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)}/>
-                    </label>
-                    
-                    <label className="navbar-btn-status"> Status
-                        <select name="status" id="status" value={status} onChange={(e)=>setStatus(e.target.value)}>
-                            <option>Complete</option>
-                            <option>Incomplete</option>
-                        </select>
-                    </label>
-                    <div className="navbar-popup-btns">
-                        <input type="submit"  onClick={handleSubmit} className="navbar-popup-btns-btn1" Add task />
-                        <button className="navbar-popup-btns-btn2"> Cancel </button>
-                    </div>
-                    
-                </div>
-            </Popup> */}
-            
-
             <div className="buttons">
-                <button className="navbar-btn">Add Task</button>
+                <button  onClick={handleButtonClick} className="navbar-btn">Add Task</button>
 
                 <select className="navbar-select" name="items" id="items">
                     <option>All</option>
@@ -74,6 +57,38 @@ function Navbar(){
                 }
 
             </div>
+            {
+                
+                popup ?
+                <div>
+                    <div className="navbar-main">
+                        <div className="navbar-popup">
+                            <div className="navbar-popup-header">
+                                <h1>Add Task</h1>
+                                <h1 onClick={closePopup}>X</h1>
+                            </div>
+                            <div className="navbar-popup-form">
+                                <form onSubmit={handleSubmit}>
+                                    <label className="navbar-btn-title"> Title
+                                        <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)}/>
+                                    </label>
+                            
+                                    <label className="navbar-btn-status"> Status
+                                        <select name="status" id="status" value={status} onChange={(e)=>setStatus(e.target.value)}>
+                                            <option>Complete</option>
+                                            <option>Incomplete</option>
+                                        </select>
+                                    </label>
+                                    <button className="navbar-popup-btns-btn1">Add Task</button>
+
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>:""
+            
+            }
 
             
         </div>
